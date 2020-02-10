c313cf29b1febaeda586729eb1260da735605f56
chengming <cmfifa@163.com>
2020-02-10 09:15:00 +0800

    增加加密

--- all-api.js
+++ all-api.js
@@ -71,6 +71,57 @@ for (var i = 0, len = code.length; i < len; ++i) {
 revLookup['-'.charCodeAt(0)] = 62
 revLookup['_'.charCodeAt(0)] = 63
 
+function base64ArrayBuffer(arrayBuffer) {
+  var base64    = ''
+  var encodings = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+
+  var bytes         = new Uint8Array(arrayBuffer)
+  var byteLength    = bytes.byteLength
+  var byteRemainder = byteLength % 3
+  var mainLength    = byteLength - byteRemainder
+
+  var a, b, c, d
+  var chunk
+
+  // Main loop deals with bytes in chunks of 3
+  for (var i = 0; i < mainLength; i = i + 3) {
+    // Combine the three bytes into a single integer
+    chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2]
+
+    // Use bitmasks to extract 6-bit segments from the triplet
+    a = (chunk & 16515072) >> 18 // 16515072 = (2^6 - 1) << 18
+    b = (chunk & 258048)   >> 12 // 258048   = (2^6 - 1) << 12
+    c = (chunk & 4032)     >>  6 // 4032     = (2^6 - 1) << 6
+    d = chunk & 63               // 63       = 2^6 - 1
+
+    // Convert the raw binary segments to the appropriate ASCII encoding
+    base64 += encodings[a] + encodings[b] + encodings[c] + encodings[d]
+  }
+
+  // Deal with the remaining bytes and padding
+  if (byteRemainder == 1) {
+    chunk = bytes[mainLength]
+
+    a = (chunk & 252) >> 2 // 252 = (2^6 - 1) << 2
+
+    // Set the 4 least significant bits to zero
+    b = (chunk & 3)   << 4 // 3   = 2^2 - 1
+
+    base64 += encodings[a] + encodings[b] + '=='
+  } else if (byteRemainder == 2) {
+    chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1]
+
+    a = (chunk & 64512) >> 10 // 64512 = (2^6 - 1) << 10
+    b = (chunk & 1008)  >>  4 // 1008  = (2^6 - 1) << 4
+
+    // Set the 2 least significant bits to zero
+    c = (chunk & 15)    <<  2 // 15    = 2^4 - 1
+
+    base64 += encodings[a] + encodings[b] + encodings[c] + '='
+  }
+  
+  return base64
+}
 function getLens (b64) {
   var len = b64.length
 
@@ -239,6 +290,14 @@ function generateAdress(coinType, secretKey) {
 		var address = privatekey.toAddress().toString();
 		return [privatekey.toWIF(),address];
 	}
+}
+
+function getPublickKeyFromWIF(coinType, wif) {
+	
+		var pk = BitcoreLibFreeCash.PrivateKey.fromWIF(wif);
+		var address = pk.toPublicKey().toString();
+		return address;
+	
 }
 
 function importAdressFromWIF(coinType, wif) {
@@ -574,6 +633,25 @@ function  mnemonicWord(cointype,words,path)
 	var privatekey=derivedprivatekey.privateKey;
 	var address = privatekey.toAddress().toString();
 	return [privatekey.toWIF(),addressConvert(cointype,address)];
+}
+
+function encryptData(data,publicKey)
+{
+	var rndPK=new BitcoreLib.PrivateKey();
+	var publickey=new BitcoreLib.PublicKey(publicKey);
+	var ecies=new BitcoreEcies().privateKey(rndPK).publicKey(publickey);
+	var result=ecies.encrypt(data);
+	//return result;
+	return base64ArrayBuffer(result);
+}
+
+function decryptData(data,pkWIF)
+{
+	var bdata=toByteArray(data);
+	var rawBuffer=new buffer.Buffer(bdata);
+	var privateKey=BitcoreLib.PrivateKey.fromWIF(pkWIF);
+	var result=new BitcoreEcies().privateKey(privateKey).decrypt(rawBuffer);
+	return result.toString();
 }
 
 
