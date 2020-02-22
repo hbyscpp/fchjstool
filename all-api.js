837f45fdf23fbdc65803ff8bb952c3a4e2c9a985
chengming <cmfifa@163.com>
2020-02-22 17:22:02 +0800

    修改feip04

--- all-api.js
+++ all-api.js
@@ -1,4 +1,34 @@
 
+
+/**
+	时间格式化
+**/
+Date.prototype.formatDate = function(fmt){
+  var o = {
+    "M+" : this.getMonth()+1,                 //月份
+    "d+" : this.getDate(),                    //日
+    "h+" : this.getHours(),                   //小时
+    "m+" : this.getMinutes(),                 //分
+    "s+" : this.getSeconds(),                 //秒
+    "q+" : Math.floor((this.getMonth()+3)/3), //季度
+    "S"  : this.getMilliseconds()             //毫秒
+  };
+
+  if(/(y+)/.test(fmt)){
+    fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+  }
+        
+  for(var k in o){
+    if(new RegExp("("+ k +")").test(fmt)){
+      fmt = fmt.replace(
+        RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));  
+    }       
+  }
+
+  return fmt;
+}
+
+
 /**
 	字符串编程utf-8字节
 **/
@@ -760,25 +790,39 @@ function encodeFEIP003(username,tags,op)
 }
 
 //protocol FEIP004
-
-function encodeFEIP004(op)
-{
-	
+//stime date类型
+//etime date类型
+function encodeFEIP004(op,stime,etime)
+{   
+	var stimestr=''
+	if (stime !== null && stime !== undefined && stime !== '') 
+	{
+		stimestr=stime.formatDate("yyyy-MM-dd hh:mm:ss")
+	}
+	var etimestr=''
+	if (etime !== null && etime !== undefined && etime !== '') 
+	{
+		etimestr=etime.formatDate("yyyy-MM-dd hh:mm:ss")
+	}
 	
 	if(op==1)
 	{
-		return "FEIP|4|1|equal"
+		return "FEIP|4|1|equal|"+stimestr+"|"+etimestr;
 	}
 	if(op==2)
 	{
-		return "FEIP|4|1|replace"
+		return "FEIP|4|1|replace|"+stimestr+"|"+etimestr;
 	}
 	if(op==3)
 	{
-		return "FEIP|4|1|backup"
+		return "FEIP|4|1|backup|"+stimestr+"|"+etimestr;
 	}
 	if(op==4)
 	{
-		return "FEIP|4|1|combine"
+		return "FEIP|4|1|combine|"+stimestr+"|"+etimestr;
+	}
+	if(op==5)
+	{
+		return "FEIP|4|1|master|"+stimestr+"|"+etimestr;
 	}
 }
